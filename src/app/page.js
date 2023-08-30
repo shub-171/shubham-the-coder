00ad353009d0b1d7ac8b6a8628d48ab8403adae4
shub-171 <shubhamagrawal14497@gmail.com>
2023-08-30 20:37:14 +0530

    s

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,9 @@ export default function Home() {
     <div className="bgImg" style={{
       backgroundImage: `url(${bgImg.src})`,
       width: '100%',
-      height: '100%',
+      minHeight: '100vh',
+      backgroundSize:'cover',
+      backgroundAttachment:'fixed',
     }}>
     <div className="container">
       <Content />

ab1009d606d1749baf6b72614652b03fdde8b2f9
shub-171 <shubhamagrawal14497@gmail.com>
2023-08-30 20:26:06 +0530

    s

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="description" content="Explore free frontend codes, templates, and web development resources. Discover hand-picked Amazon deals and discounts, get 20% off on Hostinger hosting, and explore web development services. Learn frontend and WordPress development through our YouTube channel." />
+        <meta name="keywords" content="frontend codes, templates, web development resources, Amazon deals, discounts, Hostinger hosting, web development services, frontend development, WordPress development, YouTube tutorials" />
+        <link rel="canonical" href="https://shubhamthecoder.vercel.app" />
+
+        <meta property="og:title" content="Shubham The Coder | Web Developer" />
+        <meta property="og:description" content="Explore free frontend codes, templates, and web development resources. Discover hand-picked Amazon deals and discounts, get 20% off on Hostinger hosting, and explore web development services. Learn frontend and WordPress development through our YouTube channel." />
+        <meta property="og:image" content="./opengraph-image.png" />
+        <meta property="og:url" content="https://shubhamthecoder.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Shubham The Coder | Web Developer" />
+        <meta name="twitter:description"
+          content="Explore free frontend codes, templates, and web development resources. Discover hand-picked Amazon deals and discounts, get 20% off on Hostinger hosting, and explore web development services. Learn frontend and WordPress development through our YouTube channel." />
+        <meta name="twitter:image" content="./twitter-image.png" />
+        <meta name="twitter:url" content="https://shubhamthecoder.vercel.app" />
+
+        <meta name="robots" content="index, follow" />
+        <title>Shubham The Coder | Web Developer</title>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
